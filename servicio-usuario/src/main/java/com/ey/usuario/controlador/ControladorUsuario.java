28236261d921759bcf767518965331df188945ea
Marcelo Poblete <marceloapm03@gmail.com>
2023-08-11 08:57:05 -0400

    Corección ControladorUsuario

--- servicio-usuario/src/main/java/com/ey/usuario/controlador/ControladorUsuario.java
+++ servicio-usuario/src/main/java/com/ey/usuario/controlador/ControladorUsuario.java
@@ -24,7 +24,7 @@ public class ControladorUsuario {
 	@PostMapping("/registro")
 	public RespuestaRegistro registrarUsuario(
 			@RequestBody UsuarioRegistro usuario) {
-		return servicioUsuario.c(usuario);
+		return servicioUsuario.registrarUsuario(usuario);
 	}
 	
 }
